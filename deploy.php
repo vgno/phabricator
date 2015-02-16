5047a8c656d50b66704ecac053d69825ab95ed42
Erland Wiencke <erland.wiencke@vg.no>
2015-02-16 11:57:40 +0100

    Changed the deploy script (removed the composer dependencies)

--- deploy.php
+++ deploy.php
@@ -1,12 +1,21 @@
 <?php
 
-require 'recipe/composer.php';
+require 'recipe/common.php';
 
 server('development-server', 'phabricator-dev.int.vgnett.no', 22)
-    ->path('/services/apache/phabricator-dev/phabricator-new')
+    ->path('/services/applications/phabricator-dev/phabricator')
     ->user('erlwienc')
     ->pubKey();
 
 stage('dev', array('development-server'), array('branch' => 'master'), true);
 
 set('repository', 'git@github.com:vgno/phabricator.git');
+
+task('deploy', [
+    'deploy:start',
+    'deploy:prepare',
+    'deploy:update_code',
+    'deploy:symlink',
+    'cleanup',
+    'deploy:end'
+])->desc('Deploy your project');
