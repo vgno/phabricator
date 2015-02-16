5b5a3b52521b3e8b6531ae15db54c401ef84ffe3
Erland Wiencke <erland.wiencke@vg.no>
2015-02-16 12:28:28 +0100

    Fixed the config ignore pattern

--- deploy.php
+++ deploy.php
@@ -10,7 +10,14 @@ server('development-server', 'phabricator-dev.int.vgnett.no', 22)
 stage('dev', array('development-server'), array('branch' => 'master'), true);
 
 set('repository', 'git@github.com:vgno/phabricator.git');
-set('shared_dirs', ['conf']);
+set('shared_dirs', ['conf/custom']);
+set('shared_files',
+    ['/conf/local/local.json',
+    'conf/local/ENVIRONMENT',
+    'conf/local/VERSION',
+    'conf/keys/device.pub',
+    'conf/keys/device.key',
+    'conf/keys/device.id']);
 
 
 task('deploy', [
@@ -18,6 +25,7 @@ task('deploy', [
     'deploy:prepare',
     'deploy:update_code',
     'deploy:symlink',
+    'deploy:shared',
     'cleanup',
     'deploy:end'
 ])->desc('Deploy your project');
