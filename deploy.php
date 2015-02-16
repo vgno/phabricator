ff526e17fa55ba41a15eced669bfa6af782fb1c5
Erland Wiencke <erland.wiencke@vg.no>
2015-02-16 12:28:28 +0100

    Fixed the config ignore pattern

--- deploy.php
+++ deploy.php
@@ -12,12 +12,12 @@ stage('dev', array('development-server'), array('branch' => 'master'), true);
 set('repository', 'git@github.com:vgno/phabricator.git');
 set('shared_dirs', ['conf']);
 
-
 task('deploy', [
     'deploy:start',
     'deploy:prepare',
     'deploy:update_code',
     'deploy:symlink',
+    'deploy:shared',
     'cleanup',
     'deploy:end'
 ])->desc('Deploy your project');
